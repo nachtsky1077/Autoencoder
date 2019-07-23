e4ebfd64e7efd32da5103974674714d19829f79e
nachtsky1077 <nachtsky1077@users.noreply.github.com>
2019-07-24 00:51:31 +0800

    fix visualization bug

--- visualization.py
+++ visualization.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 import argparse
 from simple_autoencoder import autoencoder, get_model_by_name
 
-markers = ['.', 'o', 'v', '^', '1', 's', 'h', 'x', 's']
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+markers = ['.', 'v', '^', '1', 's', 'h', 'x', 's']
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
 # visualize a list of 2-dimensional data with label
 def visualize(data, label, savefig=False):
     fig, ax = plt.subplots()
     categories = dict()
     for i, one_label in enumerate(label):
-        if not one_label in categories:
-            categories[one_label] = ([], [])
-        categories[one_label][0].append(data[i][0])
-        categories[one_label][1].append(data[i][1])
-    for i, one_label in enumerate(categories):
+        if not one_label.item() in categories:
+            categories[one_label.item()] = ([], [])
+        categories[one_label.item()][0].append(data[i][0])
+        categories[one_label.item()][1].append(data[i][1])
+    i = 0
+    
+    for one_label in categories:
         curr_symbol = (markers[int(one_label) // len(markers)], colors[int(one_label) % len(colors)])
         points = categories[one_label]
-        ax.scatter(x=points[0], y=points[1], marker=curr_symbol[0], c=[curr_symbol[1]] * len(points[0]))
-
+        ax.scatter(x=points[0], y=points[1], label=one_label, marker=curr_symbol[0], c=[curr_symbol[1]] * len(points[0]))
+    ax.legend()
     if not savefig:
         plt.show(fig)
     else:
